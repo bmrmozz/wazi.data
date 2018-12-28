ad67fddda6647ef5463e41eb39b71643da92ffd3
bmrmozz <bmrmozz@gmail.com>
2018-12-28 20:56:56 +0200

    additions

---
+++ src/wazi.data.core/common/clients/mongo/MongoClientSettings.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wazi.data.core.drivers;
+
+namespace wazi.data.core.common.clients.mongo {
+    public class MongoClientSettings : ObjectClientSettings {
+        public MongoClientSettings() {
+            Servers = new List<MongoServerAddress>();
+        }
+
+        public List<MongoServerAddress> Servers { get; set; }
+
+        public override string GetConnectionString() {
+            return $"mongodb://{string.Join(",", Servers.Select(s => $"{s.Host}:{s.Port}"))}";
+        }
+    }
+}
---
+++ src/wazi.data.models/models/DataObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wazi.data.models.models {
+
+    /// <summary>
+    /// A data object base class which is used for generic data object to be stored into the data collections
+    /// </summary>
+    public abstract class DataObject : ConfigurationItem {
+        public DataObject() {
+
+        }
+    }
+}
